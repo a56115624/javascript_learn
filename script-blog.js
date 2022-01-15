9e7ac13760fa9021b62aa9dbd60a3322a2b6ffa8
shane <qaz56115624@gmail.com>
2022-01-15 11:52:55 +0800

    Ok

--- script-blog.js
+++ script-blog.js
@@ -1,1 +1,19 @@
 // 部落格
+var title = document.getElementById("title");
+var content = document.getElementById("content");
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+
+
+btn.addEventListener("click",function(){
+    //console.log(list.innerHTML);
+    list.innerHTML = list.innerHTML+`
+    <div class="article">
+    <h2>${title.value}</h2>
+    <p>${content.value}</p>
+    <hr/>
+    </div>
+    `;
+    title.value ="";
+    content.value = "";
+})
